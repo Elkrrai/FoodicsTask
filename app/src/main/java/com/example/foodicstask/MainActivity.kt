590d3edd24af11647fc11752984b5c25c0196726
Elkrrai <elkrrai@lamsaworld.com>
2025-02-07 16:48:29 +0200

    update gradle

--- app/src/main/java/com/example/foodicstask/MainActivity.kt
+++ app/src/main/java/com/example/foodicstask/MainActivity.kt
@@ -19,7 +19,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             FoodicsTaskTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
                     Greeting(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
